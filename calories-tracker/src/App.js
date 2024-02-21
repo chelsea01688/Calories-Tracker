67e313d9107d934bc0b31569a50e08d57dde4265
chelsea01688 <chelsealau0168@gmail.com>
2024-02-20 23:53:39 -0500

    created the calories tracker box

---
+++ calories-tracker/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import CaloriesTrackerBox from './components/CaloriesTrackerBox';
+
+function App() {
+  return (
+    <div className="App">
+      <CaloriesTrackerBox />
+    </div>
+  );
+}
+
+export default App;
---
+++ calories-tracker/src/components/CaloriesTrackerBox.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import '../components/CaloriesTrackerBox.css'
+
+function CaloriesTrackerBox() {
+  return (
+    <div class='customBox'>
+        <div>
+            <h1>Calories Tracker App</h1>
+        </div>
+        
+        <div>
+            <h3>Total Calcories: <span>0</span></h3>
+        </div>
+    </div>
+  )
+}
+
+export default CaloriesTrackerBox
