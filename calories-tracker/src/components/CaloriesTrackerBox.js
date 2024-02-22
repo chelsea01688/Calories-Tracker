7885bae76f7d372b0cecd5eef112ad2c03824ee8
Chelsea Lau <146859678+chelsea01688@users.noreply.github.com>
2024-02-21 19:38:23 -0500

    Update CaloriesTrackerBox.js

--- calories-tracker/src/components/CaloriesTrackerBox.js
+++ calories-tracker/src/components/CaloriesTrackerBox.js
@@ -19,7 +19,8 @@ function CaloriesTrackerBox() {
     //create a new array newMeals containing the updated list of meals
     const newMeals = oldMeals.concat(meal);
     //updates the state with the new list
-    setMeals(newMeals)
+    setMeals(newMeals);
+    localStorage.setItem("meals", JSON.stringify(newMeals));
 
     //clears the input fields
     setMealName("");
@@ -32,38 +33,48 @@ function CaloriesTrackerBox() {
     const newMeals = oldMeals.filter((meal) => meal.id !== id);
 
     setMeals(newMeals);
+    localStorage.setItem("meals", JSON.stringify(newMeals));
   }
+
+  const deleteAllMeals = () => {
+    setMeals([]);
+    localStorage.clear();
+  }
+
+  const total = meals.map((meal) => meal.calories).reduce((acc, value) => acc + +value, 0);
+  console.log(total)
 
   return (
     <div className='customBox'>
       <div className='header'>
         <h1>Calories Tracker App</h1>
-        <button className='delete'>Delete All</button>
+        <button className='delete' onClick={() => deleteAllMeals()}><i class="ri-close-line" style={{fontSize: '17px'}}></i>Delete All</button>
       </div>
       <div className='totalCalories'>
-        <h3>Total Calories: <span>0</span></h3>
+        <h3>Total Calories: <span>{total}</span></h3>
       </div>
       <div className='add'>
         <input type='text'
         placeholder='Meal'
-        style={{ marginRight: '80px' }}
+        style={{ marginRight: '80px' , padding: '6px', fontSize: '15px'}}
         value={mealName}
         onChange={(e)=>setMealName(e.target.value)}
         />
         <input type='number'
         placeholder='Calories'
+        style={{ padding: '6px', fontSize: '15px'}}
         value={calories}
         onChange={(e)=>setCalories(e.target.value)}
         min={0}
         />
       </div>
-      <button onClick={addMeals} className='add'>Add Meal</button>
+      <button onClick={addMeals} className='addMeal'><i class="ri-add-line" style={{fontSize: '17px'}}></i>Add Meal</button>
       <div className='mealListContainer'>
         {meals.map((meal, index) => (
           <div key={index} className='mealListContainer_wrapper'>
-            <div>{`${meal.mealName} : ${meal.calories}`}</div>
+            <div>{`${meal.mealName} : ${meal.calories}`+ " calories"}</div>
             <div>
-              <button className='deleteMeal' onClick={() => deleteMeals(meal.id)}>Delete Meal</button>
+              <button className='delete' onClick={() => deleteMeals(meal.id)}>Delete Meal</button>
             </div>
           </div>
         ))}
